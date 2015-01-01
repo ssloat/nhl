f18eb459cead39db048c97ab46949fb1e7171abb
Stephen Sloat <stephenlsloat@gmail.com>
2014-12-31 19:46:03 -0600

    write to sqlite - no temp files

--- perl/nhl/fantasyteams.pl
+++ perl/nhl/fantasyteams.pl
@@ -1,10 +1,28 @@
+use strict;
+use warnings;
+
+use DateTime;
 use WWW::Mechanize;
 use Data::Dumper;
 use List::MoreUtils 'uniq';
+use DBI;
 
-open my $fh, '<', 'espncreds.txt';
-my @creds = <$fh>;
-chomp(@creds);
+my $ymd = DateTime->today(time_zone => 'America/Chicago')->ymd;
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$ARGV[0]","","", {});
+$dbh->do("delete from fantasyteams where date='$ymd'");
+
+my $sth = $dbh->prepare("select id from owners");
+$sth->execute();
+my @ids = map {$_->[0]} @{ $sth->fetchall_arrayref() };
+
+$sth = $dbh->prepare("select username, password from espncreds");
+$sth->execute();
+my @creds = @{ $sth->fetchall_arrayref()->[0] };
+
+$sth = $dbh->prepare('select name, id from players');
+$sth->execute();
+my %players = map {$_->[0] => $_->[1]} @{ $sth->fetchall_arrayref() };
 
 my $mech = WWW::Mechanize->new;
 $mech->get('https://r.espn.go.com/espn/memberservices/pc/login');
@@ -12,15 +30,16 @@ $mech->form_name('form-signin');
 $mech->set_fields(username => $creds[0], password => $creds[1]);
 $mech->click;
 
-
-my @teams = qw/JENN WILS RAIN BMB ELLN SIEB PBX SLOAT LEON PAUL/;
-
-for my $i (0..$#teams) {
-    my $ii = $i+1;
-    my $html = $mech->get("http://games.espn.go.com/fhl/clubhouse?leagueId=44843&teamId=$ii&seasonId=2015")->decoded_content;
+for my $id (@ids) {
+    my $html = $mech->get("http://games.espn.go.com/fhl/clubhouse?leagueId=44843&teamId=$id&seasonId=2015")->decoded_content;
 
     while ($html =~ m#playerId="\d+".*?>([a-zA-Z.-]+ [ 'a-zA-Z.-]+?)<#g) {
-        print "$teams[$i]:$1\n";
+        my $name = lc($1);
+        $name =~ s/['.]//g;
+        $name =~ s/ /-/g;
+
+        next if !defined $players{$name};
+        $dbh->do("insert into fantasyteams(owner_id, player_id, date) values ($id, '$players{$name}', '$ymd')");
     }
 }
 
--- perl/nhl/gamelogs.pl
+++ perl/nhl/gamelogs.pl
@@ -2,16 +2,37 @@ use strict;
 use warnings;
 
 use DateTime;
+use DBI;
 use List::MoreUtils 'uniq';
 use WWW::Mechanize;
+use Readonly;
 
-my $mech = WWW::Mechanize->new;
+Readonly my $MECH => WWW::Mechanize->new;
+Readonly my $DBH  => DBI->connect("dbi:SQLite:dbname=$ARGV[0]","","", {});
 
-my $start = _date($ARGV[0]);
-my $end   = _date($ARGV[1] // $ARGV[0]);
+MAIN: {
+    my $start = _date($ARGV[1]);
+    my $end   = _date($ARGV[2] // $ARGV[1]);
 
-for (; $start <= $end; $start->add(days => 1)) {
-    scoreboard($start->ymd(q{}));
+    my $start_ymd = $start->ymd;
+    my $end_ymd   = $end->ymd;
+    my $sql = <<HERE;
+delete from gamelogs 
+where exists (
+    select 1 from games where gamelogs.game_id = games.id
+        and games.date  >= '$start_ymd' and games.date <= '$end_ymd'
+)
+HERE
+    $DBH->do($sql);
+    $DBH->do("delete from games where date >= '$start_ymd' and date <= '$end_ymd'");
+
+    my $sth = $DBH->prepare("select id from players");
+    $sth->execute();
+    my %players = map {$_->[0] => 1} @{$sth->fetchall_arrayref()};
+
+    for (; $start <= $end; $start->add(days => 1)) {
+        scoreboard($start, \%players);
+    }
 }
 
 sub _date {
@@ -24,9 +45,11 @@ sub _date {
 }
 
 sub scoreboard {
-    my ($ymd) = @_;
+    my ($date, $players) = @_;
 
-    my $html = $mech->get("http://scores.espn.go.com/nhl/scoreboard?date=$ymd")->decoded_content;
+    print $date->ymd . "\n";
+    my $ymd  = $date->ymd(q{});
+    my $html = $MECH->get("http://scores.espn.go.com/nhl/scoreboard?date=$ymd")->decoded_content;
 
     my %winners = map {$_ => 1} 
             ($html =~ m#>W: <.*?><a href="http://espn.go.com/nhl/player/_/id/(\d+)/#g);
@@ -42,53 +65,93 @@ sub scoreboard {
 
     for my $gameid (uniq @gameids) {
         sleep 2;
-        game($gameid, \%winners);
+        game($gameid, $date, $players, \%winners);
     }
 }
 
 sub game {
-    my ($gameid, $winners) = @_;
+    my ($gameid, $date, $players, $winners) = @_;
 
-    my $html = $mech->get("http://scores.espn.go.com/nhl/boxscore?gameId=$gameid")->decoded_content;
+    my $html = $MECH->get("http://scores.espn.go.com/nhl/boxscore?gameId=$gameid")->decoded_content;
 
     my ($title) = ($html =~ m#<title>(.*)</title>#);
     my @teams = ($title =~ m#(.*) vs. (.*) - Boxscore#);
-    print "Title: ($gameid) $title\n";
+    $DBH->do(sprintf "insert into games (id, date, title) values ($gameid, '%s', '$title')", $date->ymd);
 
     my $ppps = power_play($html);
     my ($pstats, $gstats) = ($html =~ /Player Summary(.*)Goaltending Summary(.*)Shots On Goal/);
 
-    my $i = -1;
-    my @rows = split "</tr>", $gstats;
-    for my $row (@rows) {
-        next if $row !~ m#espn.go.com/nhl/player/_/id#;
-        $row =~ s#<td .*?>#<td>#g;
-        $row =~ s#<tr .*?>##;
-        $row =~ s#</td>##g;
+    for my $conf ([$pstats, \&_skater_insert], [$gstats, \&_goalie_insert]) {
+        my ($stats, $insert_cb) = @$conf;
 
-        my @stats = split "<td>", $row;
-        my $head = shift @stats;
-        $i++ if $head;
-        my ($id, $name) = ($stats[0] =~ m#espn.go.com/nhl/player/_/id/(\d+)/([^"]+)#);
-        my $win = defined $winners->{$id} ? 1 : 0;
-        print join(q{, },  $gameid, $teams[$i], $id, $name, 'G', $win, @stats[1..$#stats]) .  "\n";
+        my $i = -1;
+        my @rows = split "</tr>", $stats;
+        for my $row (@rows) {
+            next if $row !~ m#espn.go.com/nhl/player/_/id#;
+
+            my ($id, $name, $pos, @stats) = _parse_stats_row($row);
+            my $head = shift @stats;
+            $i++ if $head;
+
+            if (!defined $players->{$id}) {
+                $DBH->do("insert into players (id, name, pos, team) values ($id, '$name', '$pos', '$teams[$i]')");
+                $players->{$id} = 1;
+            }
+
+            shift @stats;
+            $insert_cb->($gameid, $teams[$i], $id, $name, $pos, $winners, $ppps, @stats);
+        }
     }
 
-    $i = -1;
-    @rows = split "</tr>", $pstats;
-    for my $row (@rows) {
-        next if $row !~ m#espn.go.com/nhl/player/_/id#;
-        $row =~ s#<td .*?>#<td>#g;
-        $row =~ s#<tr .*?>##;
-        $row =~ s#</td>##g;
-
-        my @stats = split "<td>", $row;
-        my $head = shift @stats;
-        $i++ if $head;
-        my ($id, $name, $pos) = ($stats[0] =~ m#espn.go.com/nhl/player/_/id/(\d+)/([^"]+).* ([A-Z]+) ?$#);
-        print join(q{, }, $gameid, $teams[$i], $id, $name, $pos, @stats[1..$#stats], ($ppps->{$name} // 0)) . "\n";
-    }
 }
+
+sub _goalie_insert {
+    my ($gameid, $team, $id, $name, $pos, $winners, $ppps, @stats) = @_;
+
+    my ($sa, $ga, $saves, $sv_perc, $toi, $pim) = @stats;
+
+    my $win = defined $winners->{$id} ? 1 : 0;
+
+    my ($min, $sec) = split /:/, $toi;
+    $toi = 60*$min + $sec;
+
+    my $sql = <<HERE;
+insert into gamelogs (player_id, game_id, wins, saves, ga, mins)
+values ($id, $gameid, $win, $saves, $ga, $toi)
+HERE
+    $DBH->do($sql);
+}
+
+sub _skater_insert {
+    my ($gameid, $team, $id, $name, $pos, $winners, $ppps, @stats) = @_;
+
+    my ($g, $a, $pm, $sog, $ms, $bs, $pn, $pim, $ht, $tk, $gv, 
+        $shf, $toi, $pp, $sh, $ev, 
+        $fw, $fl, $fper) = @stats;
+
+    my $ppp = $ppps->{$name} // 0;
+
+    my $sql = <<HERE;
+insert into gamelogs (player_id, game_id, goals, assists, ppp, plusminus, pim, sog, hits, blocks)
+values ($id, $gameid, $g, $a, $ppp, $pm, $pim, $sog, $ht, $bs)
+HERE
+    $DBH->do($sql);
+}
+
+sub _parse_stats_row {
+    my ($row) = @_;
+
+    $row =~ s#<td .*?>#<td>#g;
+    $row =~ s#<tr .*?>##;
+    $row =~ s#</td>##g;
+
+    my @stats = split "<td>", $row;
+    my ($id, $name, undef, $pos) = ($stats[1] =~ m#espn.go.com/nhl/player/_/id/(\d+)/([^"]+).*?( ([A-Z]+) ?)?$#);
+    $pos //= 'G';
+
+    return ($id, $name, $pos, @stats);
+}
+
 
 sub power_play {
     my ($str) = @_;
